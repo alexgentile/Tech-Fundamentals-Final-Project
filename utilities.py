78eab34b080179e1d0070b0c82e3b856583d15bd
alexgentile <agentile123@gmail.com>
2018-12-12 13:02:38 -0500

    Performed various types of data modeling.

--- utilities.py
+++ utilities.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 def gen_sub(df, col1, col2, filename=''):
     '''
@@ -9,8 +9,20 @@ def gen_sub(df, col1, col2, filename=''):
     Returns a pandas dataframe with 2 columns, col1 and col2 from df.
     '''
     newDF = df[[col1,col2]]
+    newDF = newDF.rename(index=str, columns={col1:'Id', col2:'SalePrice'})
     if filename != '':
         # output the new dataframe to location filename
         newDF.to_csv(filename, index=False)
     return newDF
 
+
+def rmsle(y_pred, y_test):
+    '''
+    This function takes in a list of predicted y values and a list of ground truth y values. It then computes the 
+    Root Mean Squared Logarithmic Error, or RMSLE. 
+    
+    This function implements this equation:
+    sqrt(mean(((1+y_pred) - (1+y_test))^2))
+    '''
+    assert len(y_test) == len(y_pred)
+    return np.sqrt(np.mean((np.log(1+y_pred) - np.log(1+y_test))**2))
